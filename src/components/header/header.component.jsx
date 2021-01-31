32befa3c205a8d096a7583f923bdd2c8d89ca9e5
Asif <asifaly310@gmail.com>
2021-02-01 00:34:33 +0500

    cart toggle functionality added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './header.style.scss';
 import {Link} from 'react-router-dom';
 
-import {ReactComponent as Logo} from '../../assets/crown.svg'
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 import {auth} from '../../firebase/firebase-utils';
 import {connect} from 'react-redux';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'; 
 
-
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
            <Logo  className='logo'/>
@@ -32,13 +33,19 @@ const Header = ({currentUser}) => (
                 </Link>
             )
             }
-
+             <CartIcon />
         </div>
+         {
+         hidden ? null:
+         <CartDropdown />
+         }
+       
     </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser:state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden} }) => ({
+  currentUser,
+  hidden
 });
 
 
